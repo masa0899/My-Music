195cefa260ab34735b4586d94e4a7d9499e48821
村上将也 <murakamimasaya@murakamimasaya-no-MacBook-Air.local>
2016-10-28 19:15:05 +0900

    youtube api検索機能の追加 gem追加　再度読み込み時にエラー確認

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,9 @@
 class PostsController < ApplicationController
 
+  require 'rubygems'
+  require 'google/api_client'
+  require 'trollop'
+
   def index
     @posts = Post.all.order('created_at DESC')
     @likes = Like.where(user_id: current_user)
@@ -35,7 +39,70 @@ class PostsController < ApplicationController
     end
   end
 
+  def search
+  end
+
+#youtube api
+  def search_movie
+    $search_word = params[:search_word]
+
+
+
+   opts = Trollop::options do
+     opt :q, $search_word, :type => String, :default => $search_word
+     opt :max_results, 'Max results', :type => :int, :default => 25
+   end
+
+   client, youtube = get_service
+
+   begin
+     # Call the search.list method to retrieve results matching the specified
+     # query term.
+     search_response = client.execute!(
+       :api_method => youtube.search.list,
+       :parameters => {
+         :part => 'snippet',
+         :q => opts[:q],
+         :maxResults => opts[:max_results]})
+
+     @videos = []
+     @channels = []
+     @playlists = []
+
+     # Add each result to the appropriate list, and then display the lists of
+     # matching videos, channels, and playlists.
+     search_response.data.items.each do |search_result|
+       case search_result.id.kind
+       when 'youtube#video'
+         @videos << search_result
+       when 'youtube#channel'
+         @channels << search_result
+       when 'youtube#playlist'
+         @playlists << search_result
+       end
+     end
+   rescue Google::APIClient::TransmissionError => e
+     flash.now[:error] = e.result.body
+     render action: :index
+     return false
+   end
+
+
+ end
+
+
   private
+  def get_service
+    client = Google::APIClient.new(
+      :key => 'AIzaSyA1icUIFmxUNDrrdU3qA6PQkwQbqryi9vA',
+      :authorization => nil,
+      :application_name => 'My Music',
+      :application_version => '1.0.0'
+    )
+    youtube = client.discovered_api('youtube','v3')
+    return client, youtube
+  end
+
   def post_params
     params.permit(:song_title, :singer, :youtube_url, :text)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root 'posts#index'
   resources :posts do
+    collection do
+      post 'search_movie' => 'posts#search_movie'
+      get 'search' => 'posts#search'
+    end
     resources :comments, only: [:create]
     member do
       post 'like' => 'likes#create'
