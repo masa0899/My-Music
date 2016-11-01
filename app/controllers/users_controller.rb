a0fa3f8139d237f87341b9d81f0a90d1089c2caf
村上将也 <murakamimasaya@murakamimasaya-no-MacBook-Air.local>
2016-11-01 11:48:19 +0900

    デザインとページネーション

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @nickname = current_user.nickname
-    @posts = current_user.posts
+    @posts = current_user.posts.order('created_at DESC').page(params[:page]).per(6)
   end
 
   def edit
